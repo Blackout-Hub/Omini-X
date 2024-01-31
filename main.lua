ec863ba86fefde8dce8df482ae80c6ba61c8e291
Blackout-Team <93095902+Blackout-Hub@users.noreply.github.com>
2024-01-31 11:12:32 -0300

    Up

--- main.lua
+++ main.lua
@@ -4,10 +4,10 @@ local targetPlaceID2 = 16129091908
 
 if placeID == targetPlaceID1 then
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackout-Hub/private/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackout-Hub/private/main/main.lua?token=GHSAT0AAAAAACLPEJAS5VCD3TTOX26PCWR6ZN2KIBA"))()
 
 elseif placeID == targetPlaceID2 then
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackout-Hub/private/main/autoraid.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackout-Hub/private/main/autoraid.lua?token=GHSAT0AAAAAACLPEJASZXKW3RJNHBYZROW4ZN2KKGA"))()
 
 end
